933428bab5f397d2c3e981f4f420968d16c5bb0a
Pinke2k <pinkemihaly@gmail.com>
2023-07-03 19:40:17 +0200

    init router

---
+++ frontend/src/router.jsx
@@ -0,0 +1,38 @@
+import React, { Children } from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AdminLogin from './pages/admin/AdminLogin';
+import Layout from './components/webshop/Layout/Layout';
+import { AdminProducts } from './pages/admin/AdminProducts';
+import { AdminLayout } from './components/admin/Layout/AdminLayout';
+import { Notfound } from './pages/Notfound';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/admin/login',
+        element: <AdminLogin />,
+      },
+    ],
+  },
+
+  {
+    path: '/admin',
+    element: <AdminLayout />,
+    children: [
+      {
+        path: '/admin/products',
+        element: <AdminProducts />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <Notfound />,
+  },
+]);
+
+export default router;

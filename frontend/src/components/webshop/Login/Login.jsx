3ae4fbdf09bb21960b0604e11edb3df65dfe78fc
MakoveiMiron <miron.makovei@gmail.com>
2023-07-04 20:21:41 +0200

    login form updated

--- frontend/src/components/webshop/Login/Login.jsx
+++ frontend/src/components/webshop/Login/Login.jsx
@@ -2,15 +2,14 @@ import { useState } from "react";
 export default function Login(){
     const [ formData, setFormData ] = useState({
         email: "",
-        username: "",
         password: "",
     })
     
     return(
         <>
+        <h1>Login</h1>
         <form>
             <p>Email: <input type="email" value={formData.email} onChange={e => setFormData({...formData, email: e.target.value})} required/></p>
-            <p>Username: <input type="text" value={formData.username} onChange={e => setFormData({...formData, username: e.target.value})} required/></p>
             <p>Password: <input type="password" value={formData.password} onChange={e => {setFormData({...formData, password: e.target.value}),console.log(formData)}} required/></p>
             <p><button onClick={login}>Login</button></p>
         </form>

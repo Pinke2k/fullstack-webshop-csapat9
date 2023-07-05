82123092321309f432ff873152567bf2d6121808
Pinke2k <pinkemihaly@gmail.com>
2023-07-05 20:06:55 +0200

    add port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: import.meta.env.VITE_API_URL || 3000,
+    port: 3000,
   },
 });

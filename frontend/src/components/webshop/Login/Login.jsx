3963e3ac41fc5b7e5232fcbe2c2e7fe664efb8bc
Pinke2k <pinkemihaly@gmail.com>
2023-07-05 19:19:04 +0200

    change api_url import in frontend

--- frontend/src/components/webshop/Login/Login.jsx
+++ frontend/src/components/webshop/Login/Login.jsx
@@ -1,37 +1,55 @@
-import { useState } from "react";
-import { useCookies } from "react-cookie"
-import { VITE_API_URL } from "import.meta.env"
-console.log(VITE_API_URL)
-export default function Login(){
-    const [ formData, setFormData ] = useState({
-        email: "",
-        password: "",
-    });
-    const [cookies, setCookie] = useCookies(["user"]);
-    
-    return(
-        <>
-        <h1>Login</h1>
-            <p>Email: <input type="email" value={formData.email} onChange={e => setFormData({...formData, email: e.target.value})} required/></p>
-            <p>Password: <input type="password" value={formData.password} onChange={e => {setFormData({...formData, password: e.target.value}),console.log(formData)}} required/></p>
-            <p><button onClick={login}>Login</button></p>
-        </>
-    )
+import { useState } from 'react';
+import { useCookies } from 'react-cookie';
+import { API_URL } from '../../../constants/constants';
 
-        function login(){
-            console.log(formData)
-            fetch(`${process.env.VITE_API_URL}/api/login`,{
-                method: "POST",
-                headers:{
-                    "Content-Type" : "application/json"
-                },
-                body: JSON.stringify(formData)
-            })
-            .then(resp => resp.json())
-            .then(body => {
-                setCookie("sessionID",body.sessionID);
-                
-            })
-        }
+export default function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const [cookies, setCookie] = useCookies(['user']);
 
+  return (
+    <>
+      <h1>Login</h1>
+      <p>
+        Email:{' '}
+        <input
+          type="email"
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          required
+        />
+      </p>
+      <p>
+        Password:{' '}
+        <input
+          type="password"
+          value={formData.password}
+          onChange={(e) => {
+            setFormData({ ...formData, password: e.target.value }), console.log(formData);
+          }}
+          required
+        />
+      </p>
+      <p>
+        <button onClick={login}>Login</button>
+      </p>
+    </>
+  );
+
+  function login() {
+    console.log(formData);
+    fetch(`${API_URL}/api/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((resp) => resp.json())
+      .then((body) => {
+        setCookie('sessionID', body.sessionID);
+      });
+  }
 }

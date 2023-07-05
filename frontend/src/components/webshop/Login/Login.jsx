525d7eb61373c347d4b6f703fd50be26e6918b2f
MakoveiMiron <miron.makovei@gmail.com>
2023-07-05 17:12:42 +0200

    login and register finished

--- frontend/src/components/webshop/Login/Login.jsx
+++ frontend/src/components/webshop/Login/Login.jsx
@@ -1,28 +1,36 @@
 import { useState } from "react";
+import { useCookies } from "react-cookie"
+import { VITE_API_URL } from "import.meta.env"
+console.log(VITE_API_URL)
 export default function Login(){
     const [ formData, setFormData ] = useState({
         email: "",
         password: "",
-    })
+    });
+    const [cookies, setCookie] = useCookies(["user"]);
     
     return(
         <>
         <h1>Login</h1>
-        <form>
             <p>Email: <input type="email" value={formData.email} onChange={e => setFormData({...formData, email: e.target.value})} required/></p>
             <p>Password: <input type="password" value={formData.password} onChange={e => {setFormData({...formData, password: e.target.value}),console.log(formData)}} required/></p>
             <p><button onClick={login}>Login</button></p>
-        </form>
         </>
     )
 
         function login(){
-            fetch("http://localhost:8000/api/login",{
+            console.log(formData)
+            fetch(`${process.env.VITE_API_URL}/api/login`,{
                 method: "POST",
                 headers:{
                     "Content-Type" : "application/json"
                 },
                 body: JSON.stringify(formData)
+            })
+            .then(resp => resp.json())
+            .then(body => {
+                setCookie("sessionID",body.sessionID);
+                
             })
         }
 
--- frontend/src/components/webshop/Register/Register.jsx
+++ frontend/src/components/webshop/Register/Register.jsx
@@ -10,12 +10,10 @@ export default function Login(){
     return(
         <>
         <h1>Register</h1>
-        <form>
             <p>Email: <input type="email" value={formData.email} onChange={e => setFormData({...formData, email: e.target.value})} required/></p>
             <p>Username: <input type="text" value={formData.username} onChange={e => setFormData({...formData, username: e.target.value})} required/></p>
             <p>Password: <input type="password" value={formData.password} onChange={e => {setFormData({...formData, password: e.target.value}),console.log(formData)}} required/></p>
             <p><button onClick={register}>Register</button></p>
-        </form>
         </>
     )
 

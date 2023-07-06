9bcad64fb24976247000d9da650dc5bf971732e1
Pinke2k <pinkemihaly@gmail.com>
2023-07-06 18:56:30 +0200

    add ordered-products and products-category table

---
+++ backend/src/database/models/orderedProducts-model.js
@@ -0,0 +1,21 @@
+import db from '../connection';
+
+export default {
+  createTable() {
+    const sql = `
+        CREATE TABLE IF NOT EXISTS orderedproducts(
+            order_id VARCHAR(16) NOT NULL,
+            product_id VARCHAR(16) NOT NULL,
+            amount INTEGER NOT NULL,
+            FOREIGN KEY (order_id) REFERENCES orders(id),
+            FOREIGN KEY (product_id) REFERENCES products(id)
+        )
+        `;
+    db.run(sql, (err) => {
+      if (err) {
+        console.log(`OrderedProducts table creation error: ${err.message}`);
+        throw err;
+      }
+    });
+  },
+};
---
+++ backend/src/database/models/products-category-model.js
@@ -0,0 +1,20 @@
+import db from '../connection';
+
+export default {
+  createTable() {
+    const sql = `
+        CREATE TABLE IF NOT EXISTS products_categories(
+            product_id VARCHAR(16) NOT NULL,
+            category_id VARCHAR(16) NOT NULL,
+            FOREIGN KEY (product_id) REFERENCES products(id),
+            FOREIGN KEY (category_id) REFERENCES categories(id)
+        )
+        `;
+    db.run(sql, (err) => {
+      if (err) {
+        console.log(`products_categories table creation error: ${err.message}`);
+        throw err;
+      }
+    });
+  },
+};

59004597da1ff27041c65ae7dfcb2cf938d85cfa
MakoveiMiron <miron.makovei@gmail.com>
2023-07-12 19:56:05 +0200

    cart backend

---
+++ backend/src/controllers/cart-controller.js
@@ -0,0 +1,33 @@
+import HttpError from '../utils/httpError';
+import cartsService from '../services/carts-service'
+
+export default {
+    addToCart(req, res, next){
+        const {userID, productID, amount, price} = req.body
+        if(!userID || !productID || !amount || !price) throw new HttpError('Missing required parameter', 400);
+        cartsService.addToCart({ userID, productID, amount, price })
+        .then( id => res.status(200).send(id))
+        .catch(next);
+    },
+    deleteItem(req, res, next){
+        const { id } = req.params
+        if(!id) throw new HttpError('Missing required parameter', 400);
+        cartsService.deleteItem(id)
+        .then( resp => res.status(200).send(resp))
+        .catch(next);
+    },
+    updateItem(req, res, next){
+        const { id } = req.params;
+        const { amount, price } = req.body;
+        if(!id) throw new HttpError('Missing required parameter', 400);
+        cartsService.updateItem({id, amount, price})
+        .then( resp => res.status(200).send(resp))
+        .catch(next);
+    },
+    getCart(req, res, next){
+        const { userID } = req.params
+        cartsService.getCart(userID)
+        .then(cart => res.status(200).send(cart))
+        .catch(next)
+    }
+}
--- backend/src/database/models/cart-model.js
+++ backend/src/database/models/cart-model.js
@@ -1,4 +1,5 @@
 import db from '../connection';
+import { nanoid } from 'nanoid';
 
 export default {
   createTable() {
@@ -8,6 +9,7 @@ export default {
             product_id VARCHAR(16) NOT NULL,
             user_id VARCHAR(16) NOT NULL,
             amount INTEGER NOT NULL DEFAULT 1,
+            total_price INTEGER NOT NULL DEFAULT 0,
             FOREIGN KEY (product_id) REFERENCES products(id),
             FOREIGN KEY (user_id) REFERENCES users(id)
         )
@@ -19,4 +21,64 @@ export default {
       }
     });
   },
+  addToCart({ userID, productID, amount, price }){
+    const sql = 'INSERT INTO carts (id, product_id, user_id, amount, total_price) VALUES ($id, $productID, $userID, $amount, $price)'
+    const id = nanoid(16);
+    const params = {
+      $id : id,
+      $productID: productID,
+      $userID: userID,
+      $amount: amount,
+      $price: price*amount,
+    }
+    return new Promise((resolve,reject) => {
+      db.run(sql,params, (err) => {
+        if(err) reject(err)
+        else{
+          resolve({ id })
+        }
+      })
+    } )
+  },
+  deleteItem(id){
+    const sql = 'DELETE FROM carts WHERE product_id = ?';
+    return new Promise((resolve, reject) => {
+      db.run(sql, [id], (err) => {
+        if(err) reject(err)
+        else{
+          resolve({ message: `Product with ${id} id, has been succesfully deleted from your cart`})
+        }
+      })
+    })
+  },
+  updateItem({ id, amount, price}){
+    const sql = 'UPDATE carts SET amount = $amount, total_price = $price WHERE product_id = $id'
+    const params = {
+      $amount: amount,
+      $id: id,
+      $price: price*amount
+    }
+    return new Promise((resolve, reject) => {
+      db.run(sql,params, (err) => {
+        if(err) reject(err)
+        else{
+          resolve({ id, amount })
+        }
+      })
+    })   
+  },
+  getCart(userID){
+      const sql = 'SELECT * FROM carts WHERE user_id = ?'
+      return new Promise((resolve, reject) => {
+        db.all(sql,[userID], (err,rows) => {
+          const totalPrice = rows.reduce((accumulator, currentObject) => {
+            return accumulator + currentObject.total_price;
+          }, 0);
+          if(err) reject(err)
+          else{
+            resolve({cartItems: rows, totalPrice})
+          }
+        })
+      })
+  }
 };

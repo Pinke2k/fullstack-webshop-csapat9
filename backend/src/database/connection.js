1d27a7432c466bcde497ad3aa3e479174f711815
Pinke2k <pinkemihaly@gmail.com>
2023-07-17 17:45:47 +0200

    add async database

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,9 +1,36 @@
 import Sqlite3 from 'sqlite3';
-import { resolve } from 'path';
+import path from 'path';
 
 Sqlite3.verbose();
 
-const db = new Sqlite3.Database(resolve('src', 'database', 'db', 'webshop.db'), (err) => {
+Sqlite3.Database.prototype.allAsync = function (sql, params) {
+  return new Promise((resolve, reject) => {
+    this.all(sql, params, (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  });
+};
+
+Sqlite3.Database.prototype.getAsync = function (sql, params) {
+  return new Promise((resolve, reject) => {
+    this.get(sql, params, (err, row) => {
+      if (err) reject(err);
+      else resolve(row);
+    });
+  });
+};
+
+Sqlite3.Database.prototype.runAsync = function (sql, params) {
+  return new Promise((resolve, reject) => {
+    this.run(sql, params, function (err) {
+      if (err) reject(err);
+      else resolve(this);
+    });
+  });
+};
+
+const db = new Sqlite3.Database(path.resolve('src', 'database', 'db', 'webshop.db'), (err) => {
   if (err) {
     console.log('Database connection error!');
     process.exit(1);
@@ -11,5 +38,4 @@ const db = new Sqlite3.Database(resolve('src', 'database', 'db', 'webshop.db'), 
     console.log('Db connection successful');
   }
 });
-
 export default db;
--- backend/src/database/init.js
+++ backend/src/database/init.js
@@ -6,7 +6,6 @@ import ordersModel from './models/orders-model';
 import productsCategoryModel from './models/products-category-model';
 import orderedProductsModel from './models/orderedProducts-model';
 import cartModel from './models/cart-model';
-import cartProductsModel from './models/cartProducts-model';
 
 export default function initDb() {
   db.get('PRAGMA foreign_keys = ON');
@@ -17,5 +16,4 @@ export default function initDb() {
   ordersModel.createTable();
   productsCategoryModel.createTable();
   orderedProductsModel.createTable();
-  cartProductsModel.createTable();
 }

26c54b518e7f7a262f4ab154b588465784353287
Pinke2k <pinkemihaly@gmail.com>
2023-07-19 19:23:22 +0200

    add addtocart function in productcard

--- frontend/src/components/common/ProductCard/ProductCard.jsx
+++ frontend/src/components/common/ProductCard/ProductCard.jsx
@@ -1,42 +1,35 @@
-import './ProductCard.css'
+import './ProductCard.css';
 import UpadateButton from '../../admin/AdminButtons/Update';
 import DeleteButton from '../../admin/AdminButtons/Delete';
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from 'react-router-dom';
 import { useState } from 'react';
+import { useCart } from '../../../hooks/useCart';
 
+export default function ProductCard(props) {
+  const navigate = useNavigate();
+  const { addToCart } = useCart(); // Használd az addToCart függvényt a useCart-ból
+  const [id, setId] = useState(props.id);
 
-export default function ProductCard (props){
+  function deleteProduct(id) {
+    navigate(`/admin/products/${id}/delete`);
+    console.log('props.id ', props.product.id);
+  }
 
-
-   
-	    const navigate = useNavigate();
-		const [id, setId] = useState(props.id);
-		function deleteProductt(id) {
-			navigate(`/admin/products/${id}/delete`);
-            console.log("props.id ",props.product.id)
-			
-		} 
-	
-	
-		
-return  (
+  return (
     <div className="product-card">
-    
-           {/* <img src={props.product.url} alt="image" className="image" /> */ }
-        
-         <div className="product-content">
-            <h1 className="product-title">{props.product.name}</h1>
-
-            <h3 className="product-description">{props.product.description}</h3> 
-
-            <h2 className="product-price">{props.product.price + " Ft"}</h2>
-
-            <UpadateButton id = {props.product.id} />
-            
-            <button className = "gommmb" onClick={()=> deleteProductt(props.product.id)} > Torles </button>
-
-          
-        </div>
+      {/* <img src={props.product.url} alt="image" className="image" /> */}
+      <div className="product-content">
+        <h1 className="product-title">{props.product.name}</h1>
+        <h3 className="product-description">{props.product.description}</h3>
+        <h2 className="product-price">{props.product.price + ' Ft'}</h2>
+        <UpadateButton id={props.product.id} />
+        <button className="gommmb" onClick={() => deleteProduct(props.product.id)}>
+          Törlés
+        </button>
+        <button className="gommmb" onClick={() => addToCart(props.product.id, 1)}>
+          Kosárhoz adás
+        </button>
+      </div>
     </div>
-);
+  );
 }

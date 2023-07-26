544a24c6f83b35b6d2cd16ae5f034f2086151598
mezesmihaly <mezesmihaly97@gmail.com>
2023-07-26 17:35:26 +0200

    footer-login-register css megcsinálva

--- frontend/src/components/webshop/Footer/Footer.jsx
+++ frontend/src/components/webshop/Footer/Footer.jsx
@@ -1,3 +1,27 @@
+import "./Footer.css"
+
+
+
 export function Footer() {
-  return <div>Footer</div>;
+
+
+  return(
+    <>
+        <footer className="footer">
+            <div className="info">
+                <span>Cím: ...</span>
+                <span>E-mail: ...</span>
+                <span>Telefonszám: ...</span>
+            </div>
+            <div className="contactMe">
+                <a href="/contact">Lépj velünk kapcsolatba!</a>
+            </div>
+            <div className="social">
+                <a>Facebook</a>
+                <a>Instagram</a>
+                <a>Twitter</a>
+            </div>
+        </footer>
+    </>
+)
 }

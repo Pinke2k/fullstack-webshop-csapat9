c7680f3e598688694eefefe8f3f60a5e85e983cf
Pinke2k <pinkemihaly@gmail.com>
2023-07-28 15:57:55 +0200

    add middleware to put endpoint

--- backend/src/routes/products-router.js
+++ backend/src/routes/products-router.js
@@ -9,5 +9,5 @@ router.get('/products', productsController.findAll);
 router.get('/products/:id', productsController.findOne);
 router.post('/products', uploadMiddleware.single('imageFile'), productsController.create);
 router.delete('/products/:id', /* authorizeMiddleware,*/ productsController.delete);
-router.put('/products/:productId', /* authorizeMiddleware,*/ productsController.updateProduct);
+router.put('/products/:productId',uploadMiddleware.single('imageFile'), productsController.updateProduct);
 export default router;

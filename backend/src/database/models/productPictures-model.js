754c26fe26fb0b6cfac2fd9082c5ea9bbe36edac
Pinke2k <pinkemihaly@gmail.com>
2023-07-28 11:45:48 +0200

    create product_picture model and table

--- backend/src/database/models/productPictures-model.js
+++ backend/src/database/models/productPictures-model.js
@@ -4,11 +4,12 @@ export default {
   createTable() {
     const sql = `
     CREATE TABLE IF NOT EXISTS product_pictures (
-      id INT PRIMARY KEY,
-      product_id INT NOT NULL,
-      originalname TEXT NOT NULL,
+      id INTEGER PRIMARY KEY,
+      product_id INT,
+      originalname TEXT,
       filename TEXT NOT NULL,
       path TEXT NOT NULL,
+      blurhash TEXT,
       FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE
     );
   `;
@@ -21,14 +22,15 @@ export default {
   },
 
   async addProductPicture(productId, pictureData) {
-    const { originalname, filename, path } = pictureData;
-    const sql = `INSERT INTO product_pictures (product_id, originalname, filename, path) 
-      VALUES ($productId, $originalname, $filename, $path)`;
+    const { originalname, filename, path, blurhash } = pictureData;
+    const sql = `INSERT INTO product_pictures (product_id, originalname, filename, path, blurhash) 
+      VALUES ($productId, $originalname, $filename, $path, $blurhash)`;
     const params = {
       $productId: productId,
       $originalname: originalname,
       $filename: filename,
       $path: path,
+      $blurhash: blurhash,
     };
 
     return db.runAsync(sql, params);

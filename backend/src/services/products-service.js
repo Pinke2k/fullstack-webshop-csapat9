75db322caae524f9e7cc88b297d849ea14b92dc4
Pinke2k <pinkemihaly@gmail.com>
2023-07-28 11:47:14 +0200

    add addtopicture services in create product

--- backend/src/services/products-service.js
+++ backend/src/services/products-service.js
@@ -1,13 +1,44 @@
 import productsModel from '../database/models/products-model';
+import picturesService from '../services/pictures-service';
 
 export default {
   findAll() {
     return productsModel.getAll();
   },
-  create(payload) {
-    return productsModel.create(payload).then((resp) => {
-      productsModel.addCategoriesToProduct(resp.id, payload.categoryId);
-    });
+  // async create(product, imageFile) {
+  //   console.log('termék create paylaod', payload);
+
+  //   const resp = await productsModel.create(product);
+  //   await productsModel.addCategoriesToProduct(resp.id, product.categoryId);
+
+  //   if(imageFile){
+  //     const productWithImage = await addToProductPicture()
+  //   }
+  // },
+  async create(product, imageFile) {
+    try {
+      console.log('Termék létrehozás payload:', product);
+      console.log('Termék létrehozás imageFile:', imageFile);
+      // Létrehozzuk a terméket
+      const createdProduct = await productsModel.create(product);
+
+      // Hozzáadjuk a kategóriákat a termékhez
+      await productsModel.addCategoriesToProduct(createdProduct.id, product.categoryId);
+
+      // Ha van képfájl, hozzáadjuk a képet a termékhez
+      if (imageFile) {
+        const productWithImage = await picturesService.addToProductPicture(
+          createdProduct.id,
+          imageFile,
+        );
+        return productWithImage;
+      }
+
+      return createdProduct;
+    } catch (error) {
+      console.error('Termék létrehozása során hiba:', error.message);
+      throw new Error('Termék létrehozása során hiba');
+    }
   },
   findOne(payload) {
     return productsModel.getOne(payload);
@@ -16,11 +47,10 @@ export default {
     return productsModel.delete(payload);
   },
   updateProduct(payload) {
-    console.log("itt mit ad",payload.categoryId)
-    if(!payload.categoryId){
-      return productsModel.deleteCategoriesFromProduct(payload.productId, payload.categoryId)
-    }
-    else{
+    console.log('itt mit ad', payload.categoryId);
+    if (!payload.categoryId) {
+      return productsModel.deleteCategoriesFromProduct(payload.productId, payload.categoryId);
+    } else {
       return productsModel.updateProduct(payload);
     }
   },

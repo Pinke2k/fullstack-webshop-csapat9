6b5dcda2a2716357108db3f671b8d960b1cc2ad0
Pinke2k <pinkemihaly@gmail.com>
2023-07-28 11:47:28 +0200

    modify controller

--- backend/src/controllers/products-controller.js
+++ backend/src/controllers/products-controller.js
@@ -21,15 +21,28 @@ export default {
       .catch(next);
   },
 
-  create(req, res, next) {
-    const { name, description, price, amount, categoryId } = req.body;
-  
+  // create(req, res, next) {
+  //   const { name, description, price, amount, categoryId } = req.body;
 
-    if (!price || !name) throw new HttpError('missing required parameter', 400);
-    productsService
-      .create({ name, description, price: Number(price), amount: Number(amount), categoryId })
-      .then((product) => res.status(201).send(product))
-      .catch(next);
+  //   if (!price || !name) throw new HttpError('missing required parameter', 400);
+  //   productsService
+  //     .create({ name, description, price: Number(price), amount: Number(amount), categoryId })
+  //     .then((product) => res.status(201).send(product))
+  //     .catch(next);
+  // },
+  async create(req, res, next) {
+    try {
+      // Létrehozzuk a terméket a productService segítségével
+      console.log(req.file, 'create controller req file');
+      console.log(req.formdata, 'create controller req formdata');
+      const newProduct = await productsService.create(req.body, req.file);
+      console.log(newProduct, 'új termék controller');
+
+      // Visszatérünk a létrehozott termékkel
+      res.json(newProduct);
+    } catch (err) {
+      next(err);
+    }
   },
 
   delete(req, res, next) {
@@ -42,13 +55,37 @@ export default {
   updateProduct(req, res, next) {
     const { productId } = req.params;
     const { name, description, price, amount, categoryId } = req.body;
-   
+
     productsService
       .updateProduct({ productId, name, description, price, amount, categoryId })
       .then((product) => {
-       
         res.status(200).send({ product });
       })
       .catch(next);
   },
 };
+
+// const createProduct = async (req, res, next) => {
+//   try {
+//     const newProduct = await productService.createProduct(req.body, req.file);
+//     res.json(newProduct.rows[0]);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+// const handleProductPicture = async (req, res) => {
+//   try {
+//     const productId = req.params.productId;
+//     const pictureDataArray = req.files;
+
+//     // Képek hozzáadása a termékhez a service segítségével
+//     const dbUpdates = await productPicturesService.addToProductPicture(productId, pictureDataArray);
+
+//     // Visszatérhetünk a mentett adatokkal vagy az adatbázisból kapott eredménnyel
+//     return res.status(200).json(dbUpdates);
+//   } catch (error) {
+//     console.error(error.message);
+//     return res.status(500).json({ error: 'Kép feltöltési hiba' });
+//   }
+// };

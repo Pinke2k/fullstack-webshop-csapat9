265b51286d26e210f65b778948cded97287d1422
Pinke2k <pinkemihaly@gmail.com>
2023-07-31 18:45:16 +0200

    package and productcard

--- frontend/src/components/common/ProductCard/WebshopProductCard.jsx
+++ frontend/src/components/common/ProductCard/WebshopProductCard.jsx
@@ -1,17 +1,26 @@
-import './ProductCard.css'
-;
-import { useNavigate, useParams } from "react-router-dom";
+import './ProductCard.css';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useState } from 'react';
+import { useCart } from '../../../hooks/useCart';
+import { BlurHashLazyLoading } from '../BlurhashLazyLoading/BlurhashLazyloading';
 
-
-export default function ProductCard (props){
-
-return  (
+export default function ProductCard(props) {
+  const { addToCart } = useCart();
+  const [id, setId] = useState(props.id);
+  return (
     <div className="product-card">
-        <img src={props.product.url} alt="image" className="image" />
+      <div className="image">
+        <BlurHashLazyLoading image={props.product} />
+      </div>
+      <div className="product-content">
         <h1 className="product-title">{props.product.name}</h1>
-        <h2 className="product-price">{props.product.price + " Ft"}</h2>
-        <h2 className='add-product'>Kosárba</h2>
+        <div className="lastline">
+          <div className="product-price">{props.product.price + ' Ft'}</div>
+          <button className="add-product" onClick={() => addToCart(props.product.id, 1)}>
+            Kosárhoz adás
+          </button>
+        </div>
+      </div>
     </div>
-);
+  );
 }

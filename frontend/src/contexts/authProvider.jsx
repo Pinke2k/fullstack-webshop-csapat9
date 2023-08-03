51abd354d016fabb9f8bfbbf6f77ea7716b7279f
Pinke2k <pinkemihaly@gmail.com>
2023-08-03 17:46:48 +0200

    modify auth logic

--- frontend/src/contexts/authProvider.jsx
+++ frontend/src/contexts/authProvider.jsx
@@ -1,9 +1,18 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import { createAuthInit } from '../services/auth-fetch';
+import jwtDecode from 'jwt-decode';
 
 const AuthContext = createContext({});
 
 export function AuthProvider({ children }) {
-  const [auth, setAuth] = useState({ token: localStorage.getItem('accessToken') });
+  const [auth, setAuth] = useState(createAuthInit());
+
+  useEffect(() => {
+    if (auth.accessToken) {
+      const decoded = jwtDecode(auth.accessToken);
+      setAuth((prev) => ({ ...prev, user: { ...decoded } }));
+    }
+  }, [auth.accessToken]);
 
   return <AuthContext.Provider value={{ auth, setAuth }}>{children}</AuthContext.Provider>;
 }

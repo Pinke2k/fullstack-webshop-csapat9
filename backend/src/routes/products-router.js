50af596089e0f958e4edaa9b46d3faf5d1ed9621
Pinke2k <pinkemihaly@gmail.com>
2023-08-03 17:45:21 +0200

    add protected middleware to endpoints

--- backend/src/routes/products-router.js
+++ backend/src/routes/products-router.js
@@ -2,13 +2,13 @@ import express from 'express';
 import productsController from '../controllers/products-controller';
 import authorizeMiddleware from '../middlewares/authorize-middleware';
 import uploadMiddleware from '../middlewares/multer-middleware';
+import verifyAdmin from '../middlewares/verify-admin-middleware';
 
 const router = express.Router();
 
 router.get('/products', productsController.findAll);
 router.get('/products/:id', productsController.findOne);
-router.post('/products', uploadMiddleware.single('imageFile'), productsController.create);
-router.delete('/products/:id', /* authorizeMiddleware,*/ productsController.delete);
-router.put('/products/:productId', uploadMiddleware.single('imageFile'),productsController.updateProduct,
-);
+router.post( '/products', authorizeMiddleware,verifyAdmin, uploadMiddleware.single('imageFile'), productsController.create,);
+router.delete('/products/:id', authorizeMiddleware,verifyAdmin,productsController.delete);
+router.put('/products/:productId',authorizeMiddleware,verifyAdmin,uploadMiddleware.single('imageFile'), productsController.updateProduct,);
 export default router;

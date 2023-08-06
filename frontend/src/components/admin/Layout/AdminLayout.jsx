0066309affcdc29a097d94e98349519756199426
Pinke2k <pinkemihaly@gmail.com>
2023-08-06 16:15:44 +0200

    modify admin layout

--- frontend/src/components/admin/Layout/AdminLayout.jsx
+++ frontend/src/components/admin/Layout/AdminLayout.jsx
@@ -5,32 +5,43 @@ import { NavLink } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import MainDash from '../Dashboard/MainDash';
+import { useLocation } from 'react-router-dom';
 
 export default function AdminLayout() {
   const { user } = useAuth();
+  const location = useLocation();
   console.log(user, 'user');
 
   if (user?.isAdmin === 0) {
     return <Navigate to="/" />;
   }
 
+  // return (
+  //   <>
+  //     {user?.isAdmin ? (
+  //       <>
+  //         <AdminNav />
+  //         <MainDash/>
+  //         <Outlet />
+  //       </>
+  //     ) : (
+  //       <Navigate to="/"></Navigate>
+  //     )}
+  //   </>
+  // );
+
   return (
     <>
-      {user?.isAdmin ? (
-        <>
-          <AdminNav />
-          <MainDash/>
-          <Outlet />
-        </>
-      ) : (
-        <Navigate to="/"></Navigate>
-      )}
+      <AdminNav />
+      {user.isAdmin && location.pathname === '/admin' && <MainDash />}
+      <Outlet />
     </>
   );
+}
   // return (
   //   <>
   //     <AdminNav />
   //     <Outlet />
   //   </>
   // );
-}
+
